f21719eec6d4c4049cdbe19e4a394d12598d1fa5
alexander <alexander.i.kamenev@gmail.com>
2017-06-21 17:27:02 +0300

    extend tests

--- tests.py
+++ tests.py
@@ -2,6 +2,10 @@ import pytest
 from format_price import get_format_price
 
 
+class TestClass:
+    pass
+
+
 @pytest.mark.parametrize('test_input,expected', [
     ('123', '123'),
     ('1234', '1 234'),
@@ -11,3 +15,15 @@ from format_price import get_format_price
 ])
 def test_formatting(test_input, expected):
     assert get_format_price(test_input) == expected
+
+
+@pytest.mark.parametrize('test_input,exception', [
+    (set(), TypeError),
+    (list(), TypeError),
+    (dict(), TypeError),
+    (None, TypeError),
+    (TestClass(), TypeError),
+])
+def test_exceptions(test_input, exception):
+    with pytest.raises(exception):
+        get_format_price(test_input)
